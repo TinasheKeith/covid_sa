c4eb1696761a53049686ab20c0c45d0b223e81e6
tintin.dev <tinashe.madzimbamuto@younglings.africa>
2020-05-08 11:21:51 +0200

    added widget binding for async main method call

--- lib/environments/main_common.dart
+++ lib/environments/main_common.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
 
-import 'package:covid_sa/main.dart';
+import 'package:provider/provider.dart';
 
-void mainCommon(String env) {
-  runApp(App());
+import 'package:covid_sa/main.dart';
+import 'package:covid_sa/config_reader.dart';
+import 'package:covid_sa/environments/environment.dart';
+
+String appTitle;
+
+Future<void> mainCommon(String env) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await ConfigReader.initialise();
+
+  switch (env) {
+    case Environment.dev:
+      appTitle = "COVIDSA-DEV";
+      break;
+    case Environment.prod:
+      appTitle = "COVIDSA";
+      break;
+  }
+
+  runApp(
+    Provider.value(
+      value: appTitle,
+      child: App(),
+    ),
+  );
 }
--- lib/environments/main_prod.dart
+++ lib/environments/main_prod.dart
@@ -2,5 +2,5 @@ import 'package:covid_sa/environments/environment.dart';
 import 'main_common.dart';
 
 Future<void> main() async {
-  mainCommon(Environment.prod);
+  await mainCommon(Environment.prod);
 }
