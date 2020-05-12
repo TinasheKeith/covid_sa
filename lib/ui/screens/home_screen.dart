61447e0d81dbe0549ed048ca5fc7ee8acfcb6eb6
tintin.dev <tinashe.madzimbamuto@younglings.africa>
2020-05-12 08:28:48 +0200

    added stacks animation

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:covid_sa/ui/screens/home_tab.dart';
+import 'package:covid_sa/ui/screens/menu.dart';
 import 'package:covid_sa/ui/screens/news_tab.dart';
 import 'package:covid_sa/ui/screens/stats_tab.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 
 import 'package:covid_sa/ui/widgets/app_tab.dart';
@@ -12,8 +14,10 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen>
-    with SingleTickerProviderStateMixin {
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  AnimationController _animationController;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   TabController _tabController;
 
   final List<Map> _tabs = [
@@ -39,6 +43,10 @@ class _HomeScreenState extends State<HomeScreen>
   void initState() {
     super.initState();
     _tabController = TabController(length: 4, vsync: this);
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 250),
+    );
   }
 
   @override
@@ -47,33 +55,61 @@ class _HomeScreenState extends State<HomeScreen>
     _tabController.dispose();
   }
 
+  void toggle() => _animationController.isDismissed
+      ? _animationController.forward()
+      : _animationController.reverse();
+
+  final double maxSlide = 225.0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          Provider.of<String>(context),
-        ),
-        centerTitle: true,
-        bottom: TabBar(
-          controller: _tabController,
-          indicatorColor: Theme.of(context).primaryColor,
-          indicatorSize: TabBarIndicatorSize.label,
-          indicatorWeight: 3,
-          tabs: _tabs
-              .map((tab) => AppTab(icon: tab["icon"], title: tab["title"]))
-              .toList(),
-        ),
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: <Widget>[
-          HomeTab(),
-          StatsTab(),
-          NewsTab(),
-          Center(),
-        ],
-      ),
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, _) {
+        double slide = maxSlide * _animationController.value;
+        double scale = 1 - (_animationController.value * 0.3);
+        return Stack(
+          children: [
+            Menu(),
+            Transform(
+              transform: Matrix4.identity()
+                ..translate(slide)
+                ..scale(scale),
+              alignment: Alignment.centerLeft,
+              child: Scaffold(
+                key: _scaffoldKey,
+                appBar: AppBar(
+                  leading:
+                      IconButton(icon: Icon(Icons.settings, size: 24), onPressed: toggle),
+                  title: Text(
+                    Provider.of<String>(context),
+                  ),
+                  centerTitle: true,
+                  bottom: TabBar(
+                    controller: _tabController,
+                    indicatorColor: Theme.of(context).primaryColor,
+                    indicatorSize: TabBarIndicatorSize.label,
+                    indicatorWeight: 3,
+                    tabs: _tabs
+                        .map((tab) =>
+                            AppTab(icon: tab["icon"], title: tab["title"]))
+                        .toList(),
+                  ),
+                ),
+                body: TabBarView(
+                  controller: _tabController,
+                  children: <Widget>[
+                    HomeTab(),
+                    StatsTab(),
+                    NewsTab(),
+                    Center(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
