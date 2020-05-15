fe96978f9c8e91bc4789de49f512711b5c722d3d
tintin.dev <tinashe.madzimbamuto@younglings.africa>
2020-05-15 16:38:49 +0200

    updated news cards

--- lib/ui/screens/news_tab.dart
+++ lib/ui/screens/news_tab.dart
@@ -5,7 +5,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:covid_sa/blocs/blocs.dart';
 import 'package:covid_sa/models/models.dart';
-import 'package:covid_sa/ui/widgets/image_card.dart';
 import 'package:covid_sa/ui/widgets/flare_animations/no_wifi.dart';
 
 class NewsTab extends StatelessWidget {
@@ -23,17 +22,34 @@ class NewsTab extends StatelessWidget {
       builder: (context, state) {
         if (state is NewsLoaded) {
           final NewsArticles newsArticles = state.newsArticles;
-          return ListView.builder(
-            itemCount: newsArticles.articles.length,
-            itemBuilder: (BuildContext context, int i) {
-              return ImageCard(
-                title: newsArticles.articles[i].title,
-                subtitle: newsArticles.articles[i].source.name,
-                urlToImage: newsArticles.articles[i].urlToImage,
-                onTap: () =>
-                    openArticleUrl(newsArticles.articles[i].url, context),
-              );
+          return NestedScrollView(
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return [
+                SliverAppBar(
+                  expandedHeight: 250,
+                  flexibleSpace: FlexibleSpaceBar(
+                    title: Text(
+                      newsArticles.articles[0].title,
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    titlePadding: const EdgeInsets.only(bottom: 16),
+                    background: Image.network(
+                      newsArticles.articles[0].urlToImage,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ];
             },
+            body: ListView.builder(
+              itemCount: newsArticles.articles.length,
+              itemBuilder: (BuildContext context, int i) {
+                return NewsCard(
+                  article: newsArticles.articles[i],
+                );
+              },
+            ),
           );
         }
         if (state is NewsLoading) {
@@ -54,6 +70,54 @@ class NewsTab extends StatelessWidget {
           ),
         );
       },
+    );
+  }
+}
+
+class NewsCard extends StatelessWidget {
+  final Article article;
+
+  NewsCard({this.article});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => ,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 120,
+                width: 120,
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                      topLeft: const Radius.circular(12),
+                      bottomLeft: const Radius.circular(12)),
+                  child: Image.network(
+                    article.urlToImage,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SizedBox(width: 8),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(article.title,
+                          style: Theme.of(context).textTheme.bodyText2),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
